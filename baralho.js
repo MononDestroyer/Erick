cff44508aad83cf569a9cb44ee6fdf9782f16375
MononDestroyer <145119857+MononDestroyer@users.noreply.github.com>
2023-12-08 07:55:59 -0300

    Update and rename baralho to baralho.js

---
+++ baralho.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Baralho Virtual</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Baralho Virtual</h1>
+    <button onclick="embaralhar()">Embaralhar</button>
+    <div class="baralho" id="baralho"></div>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
